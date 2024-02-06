1837ea70ecdd2344f2d3f16c50db201cb48aa8b0
20017801 <cf267@st-andrews.ac.uk>
2024-02-06 16:43:01 +0000

    Re-added tests

--- Test.hs
+++ Test.hs
@@ -19,51 +19,42 @@ prop_objectFoundInRoom obj rm =
    (obj `elem` (objects rm)) == (objectHere obj rm)
 
 
-prop_removeObjectLength :: Object -> Room -> Bool 
-prop_removeObjectLength obj rm  
- | obj `elem` (objects rm) = length (objects newRoom) == originalLength - 1
- | otherwise = length (objects newRoom) == originalLength
- where 
-    originalLength = length (objects rm)
-    newRoom = removeObject obj rm
+-- prop_removeObject :: Object -> Room -> Bool 
+-- prop_removeObject obj rm = do
+--         let origionalLength = length (objects rm)
+            
+--  | obj `elem` (objects rm) then length (objects (removeObject obj rm)) == x 
+prop_testDrink:: Object -> GameData -> Bool
+prop_testDrink obj gd
+ | carrying gd fullmug && (obj == mug || obj == coffeepot) = caffeinated updatedGD && carrying updatedGD mug
+ | otherwise = gd == updatedGD
+ where updatedGD = fst(drink obj gd)
 
-prop_removeObject :: Object -> Room -> Bool 
-prop_removeObject obj rm = obj `notElem` objects(removeObject obj rm)
+prop_testDress:: GameData -> Bool
+prop_testDress gd 
+ | getCurrentRoom gd == wardrobe && carrying gd trainers && carrying gd jeans && carrying gd hoodie = dressed updatedGD
+ | otherwise = dressed gd == dressed updatedGD
+ where updatedGD= fst(open gd)
 
+prop_testOpen :: GameData -> Bool
+prop_testOpen gd 
+ | caffeinated gd && dressed gd && locationId gd == "hall" = exits (getCurrentRoom updatedGD) ==[Exit "east" "To the east is a kitchen. " "kitchen", Exit "out" "You can go outside. " "street"]
+ | otherwise = gd == updatedGD
+ where updatedGD= fst(open gd)
 
-prop_addObjectLength :: Object -> Room -> Bool 
-prop_addObjectLength obj rm = length (objects newRoom) == originalLength + 1
- where 
-    originalLength = length (objects rm)
-    newRoom = addObject obj rm
+prop_testLights :: GameData -> Bool
+prop_testLights gd = lightsOn gd /= lightsOn updatedGD
+ where updatedGD= fst(lights gd)
 
-prop_addObject :: Object -> Room -> Bool 
-prop_addObject obj rm = obj `elem` objects(addObject obj rm)
-
-
--- prop_updateRoom :: GameData -> String -> Room
-
-prop_addToInventory :: GameData -> Object -> Bool
-prop_addToInventory gd obj = length (inventory (addInv gd obj)) == originalLength + 1
- where 
-    originalLength = length (inventory gd)
-
-prop_removeFromInventory :: GameData -> Object -> Bool
-| obj `elem` (inventory gd) = length (inventory (removeInv gd obj)) == originalLength - 1
-| otherwise = length (inventory (removeInv gd obj)) == originalLength
- where 
-    originalLength = length (inventory gd)
-
--- not going to make a test that checks the object has been added to the inventory, because since the values are arbitrary we could have multiple instances of the same object in the inventory
+prop_testBrush :: GameData -> Bool
+prop_testBrush gd 
+ | carrying gd toothbrush = brushed updatedGD == True
+ | carrying gd toothbrush == False = brushed updatedGD == brushed gd
+ where updatedGD= fst(brush gd)
 
 run = do 
     quickCheck prop_correctMoveReturn
     quickCheck prop_objectFoundInRoom
-    quickCheck prop_removeObjectLength
-    quickCheck prop_removeObject
-    quickCheck prop_addObject
-    quickCheck prop_addObjectLength
-    quickCheck prop_addToInventory
     quickCheck prop_testBrush
     quickCheck prop_testLights
     quickCheck prop_testOpen    
